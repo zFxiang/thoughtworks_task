f6b6ac01917a232b6d85cab43a8fbf35c8be9066
z <zouxiangq@qq.com>
2018-04-13 21:56:18 +0800

    完成 3.B

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -1,12 +1,26 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> map = new HashMap<>(collectionA.size());
+        Set<String> set = new HashSet<>();
+        for (String key : object.keySet()) {
+            set.addAll(object.get(key));
+        }
+        for (String key : collectionA.keySet()) {
+            Integer count = collectionA.get(key);
+            if(set.contains(key)){
+                count -= count / 3;
+            }
+            map.put(key,count);
+        }
+        return map;
     }
 }
