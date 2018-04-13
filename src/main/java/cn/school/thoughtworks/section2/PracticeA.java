dddcd0572164dfbfc5da510799821b18469d60e8
z <zouxiangq@qq.com>
2018-04-13 15:58:37 +0800

    完成 2.A

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,21 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> map = new LinkedHashMap<>();
+        for (String str : collection1) {
+            Integer count = map.get(str);
+            if(count == null){
+                count = 0;
+            }
+            count++;
+            map.put(str,count);
+        }
+        return map;
     }
 }
