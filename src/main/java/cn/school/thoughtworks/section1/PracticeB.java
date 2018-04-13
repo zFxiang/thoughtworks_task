bcdf63486db3308888a2a2f8b2249b7e7df0c6bf
z <zouxiangq@qq.com>
2018-04-13 15:50:49 +0800

    完成 1.B

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        Set<String> set1 = new LinkedHashSet<>(collection1);
+        Set<String> set2 = new HashSet<>();
+        for (List<String> list : collection2) {
+            set2.addAll(list);
+        }
+        set1.retainAll(set2);
+        return new ArrayList<>(set1);
     }
 }
