a4962918bb30fcbbc968398a4d175984c2370e7e
z <zouxiangq@qq.com>
2018-04-13 15:45:28 +0800

    完成 1.A

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,15 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        Set<String> set1 = new LinkedHashSet<>(collection1);
+        set1.retainAll(collection2);
+        return new ArrayList<>(set1);
     }
 }
