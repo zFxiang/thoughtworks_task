9d34f1225863522b9ae1b9bb69eba359a62267f9
z <zouxiangq@qq.com>
2018-04-30 11:09:11 +0800

    TDD done

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -5,10 +5,42 @@ import java.util.List;
 
 
 public class FizzBuzzGame {
+    public static final String BUZZ = "Buzz";
+    public static final String FIZZ = "Fizz";
+    public static final String WHIZZ = "Whizz";
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
+        for (int i = 1; i <= count; i++) {
+            results.add(countOff(i));
+        }
+    }
+    public String countOff(int num){
+        if (isContainsThree(num)) {
+            return FIZZ;
+        }
+        StringBuilder sb = new StringBuilder();
+        if(num % 3 == 0){
+            sb.append(FIZZ);
+        }
+        if(num % 5 == 0){
+            sb.append(BUZZ);
+        }
+        if(num % 7 == 0){
+            sb.append(WHIZZ);
+        }
+        return sb.length() > 0 ? sb.toString() : String.valueOf(num);
+    }
 
+    private boolean isContainsThree(int num) {
+        int temp = num;
+        while (temp != 0){
+            if(temp % 10 == 3){
+                return true;
+            }
+            temp /= 10;
+        }
+        return false;
     }
 
     public List<String> getResults() {
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,90 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static com.thoughtworks.FizzBuzzGame.BUZZ;
+import static com.thoughtworks.FizzBuzzGame.FIZZ;
+import static com.thoughtworks.FizzBuzzGame.WHIZZ;
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzGameTest {
+
+    private FizzBuzzGame fizzBuzzGame;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzzGame = new FizzBuzzGame();
+    }
+
+    @Test
+    public void should_return_1_when_1() {
+        assertEquals(fizzBuzzGame.countOff(1),"1");
+    }
+
     @Test
     public void should_return_Fizz_when_have_3() {
+        assertEquals(fizzBuzzGame.countOff(3), FIZZ);
+    }
+    @Test
+    public void should_return_Buzz_when_have_5(){
+        assertEquals(fizzBuzzGame.countOff(5), BUZZ);
+    }
+    @Test
+    public void should_return_Whizz_when_have_7(){
+        assertEquals(fizzBuzzGame.countOff(7), WHIZZ);
+    }
 
+    @Test
+    public void should_return_Whizz_when_have_3_and_5(){
+        assertEquals(fizzBuzzGame.countOff(15), FIZZ + BUZZ);
+    }
+
+    @Test
+    public void should_return_Whizz_when_have_5_and_7(){
+        assertEquals(fizzBuzzGame.countOff(70), BUZZ + WHIZZ);
+    }
+    @Test
+    public void should_return_Whizz_when_have_3_and_7(){
+        assertEquals(fizzBuzzGame.countOff(21), FIZZ + WHIZZ);
+    }
+    @Test
+    public void should_return_Whizz_when_have_3_5_and_7(){
+        assertEquals(fizzBuzzGame.countOff(105), FIZZ + BUZZ + WHIZZ);
+    }
+
+    @Test
+    public void should_return_Whizz_when_contain_3(){
+        assertEquals(fizzBuzzGame.countOff(35), FIZZ);
+    }
+
+    @Test
+    public void testRange() {
+        String result = "1\n" +
+                "2\n" +
+                "Fizz\n" +
+                "4\n" +
+                "Buzz\n" +
+                "Fizz\n" +
+                "Whizz\n" +
+                "8\n" +
+                "Fizz\n" +
+                "Buzz\n" +
+                "11\n" +
+                "Fizz\n" +
+                "Fizz\n" +
+                "Whizz\n" +
+                "FizzBuzz\n" +
+                "16\n" +
+                "17\n" +
+                "Fizz\n" +
+                "19\n" +
+                "Buzz";
+        List<String> stringList = Arrays.asList(result.split("\n"));
+        fizzBuzzGame.start(20);
+        assertEquals(fizzBuzzGame.getResults(),stringList);
     }
 }
